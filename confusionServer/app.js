a67cf7f193b9f376a81d149babe76c139da917c1
srinivasthomala <srinivas.1796@gmail.com>
2020-07-04 14:25:17 -0400

    File Upload

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
@@ -55,11 +56,10 @@ app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
