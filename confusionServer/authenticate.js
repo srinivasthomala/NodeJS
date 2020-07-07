aaa8e3755ea7f3ebf63e1ae752e7d95697cbc28b
srinivasthomala <srinivas.1796@gmail.com>
2020-07-06 22:31:23 -0400

    Passport Facebook

--- confusionServer/authenticate.js
+++ confusionServer/authenticate.js
@@ -8,6 +8,8 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 var config = require('./config.js');
 
+var FacebookTokenStrategy = require('passport-facebook-token');
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -49,3 +51,30 @@ exports.verifyAdmin = (this.verifyUser, (req, res, next) => {
         next(err);
     }
 });
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
--- confusionServer/config.js
+++ confusionServer/config.js
@@ -2,3 +2,12 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/confusion'
 }
+
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/confusion',
+    'facebook': {
+        clientId: '282525489530311',
+        clientSecret: 'a684936f22d190652ff2cc06c0fa3f7a'
+    }
+}
