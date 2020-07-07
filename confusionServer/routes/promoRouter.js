3de3dd9e7c074c93b9f48ca05ead67bd9eb255e8
srinivasthomala <srinivas.1796@gmail.com>
2020-07-06 21:55:55 -0400

    CORS

--- confusionServer/routes/promoRouter.js
+++ confusionServer/routes/promoRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const cors = require('./cors');
+
 const Promotions = require('../models/promotions');
 var authenticate = require('../authenticate');
 
@@ -10,7 +12,8 @@ const promoRouter = express.Router();
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
-.get((req,res,next) => {
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, (req,res,next) => {
   Promotions.find({})
   .then((promotions) => {
       res.statusCode = 200;
@@ -19,7 +22,7 @@ promoRouter.route('/')
   }, (err) => next(err))
   .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   Promotions.create(req.body)
   .then((promotion) => {
       console.log('Promotion Created ', promotion);
@@ -29,11 +32,11 @@ promoRouter.route('/')
   }, (err) => next(err))
   .catch((err) => next(err));
 })
-.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promos');
   })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   Promotions.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -44,7 +47,8 @@ promoRouter.route('/')
 });
 
 promoRouter.route('/:promoId')
-.get((req,res,next) => {
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, (req,res,next) => {
   Promotions.findById(req.params.promoId)
   .then((promotion) => {
       res.statusCode = 200;
@@ -53,11 +57,11 @@ promoRouter.route('/:promoId')
   }, (err) => next(err))
   .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
 res.statusCode = 403;
 res.end('POST operation not supported on /promos/'+ req.params.promoId);
 })
-.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   Promotions.findByIdAndUpdate(req.params.promoId, {
       $set: req.body
   }, { new: true })
@@ -68,7 +72,7 @@ res.end('POST operation not supported on /promos/'+ req.params.promoId);
   }, (err) => next(err))
   .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   Promotions.findByIdAndRemove(req.params.promoId)
   .then((resp) => {
       res.statusCode = 200;
--- confusionServer/routes/users.js
+++ confusionServer/routes/users.js
@@ -7,16 +7,18 @@ var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
 
+const cors = require('./cors');
+
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
   User.find({}).then((users) => {
     res.send(users);
     });
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
@@ -46,7 +48,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
@@ -54,7 +56,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', cors.corsWithOptions, (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
